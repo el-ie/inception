433bc6e38bce1e2c731e80dcbecd52f3badd43fc
bob <bob@core>
2023-04-16 02:09:22 +0200

    mariadb sa grand mere

--- separation/srcs/requirements/wordpress/conf/wp-config.php
+++ separation/srcs/requirements/wordpress/conf/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'tobi' );
 define( 'DB_PASSWORD', 'pass' );
 
 /** Database hostname */
-define( 'DB_HOST', 'mariadbC' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
